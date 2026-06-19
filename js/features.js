1d386166b06cd81bd5e840ea0f78a4f33b17f293
Robe Mike <robemike11355@gmail.com>
2026-06-19 08:07:40 +0300

    Written js, aid in loading components dynamically

---
+++ js/features.js
@@ -0,0 +1,5 @@
+fetch('components/features.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('features-container').innerHTML = data;
+});
---
+++ js/hero.js
@@ -0,0 +1,5 @@
+fetch('components/hero.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('hero-container').innerHTML = data;
+})
